7bcedf67f89d636fe9bcdacd76bccda108927279
lenaevd <lenaevd0kim0va@yandex.ru>
2025-05-20 23:45:15 +0300

    fixed isRead in mapping messages

--- src/main/java/com/lenaevd/advertisements/mapper/MessageMapper.java
+++ src/main/java/com/lenaevd/advertisements/mapper/MessageMapper.java
@@ -3,16 +3,18 @@ package com.lenaevd.advertisements.mapper;
 import com.lenaevd.advertisements.dto.MessageDto;
 import com.lenaevd.advertisements.model.Message;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MessageMapper {
-
-    @Mapping(source = "chat.id", target = "chatId")
-    @Mapping(source = "sender.id", target = "senderId")
-    MessageDto messageToMessageDto(Message message);
+    default MessageDto messageToMessageDto(Message message) {
+        if (message == null) {
+            return null;
+        }
+        return new MessageDto(message.getId(), message.getChat().getId(), message.getSender().getId(),
+                message.getContent(), message.getSentAt(), message.isRead());
+    }
 
     List<MessageDto> messagesToMessageDtos(List<Message> messages);
 }
